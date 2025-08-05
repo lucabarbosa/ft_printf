0215209104c7094e62adc35985ba15710e3d73d3
Luca Barbosa <lucabento.luca@gmail.com>
2025-08-05 00:46:30 -0300

    Started prototype and configs

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,16 @@
-#include "libfprintf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 00:41:28 by lbento            #+#    #+#             */
+/*   Updated: 2025/08/05 00:45:18 by lbento           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
 
 int	ft_printf(const char *, ...);
 
@@ -6,3 +18,22 @@ int	ft_printf(const char *, ...)
 {
 
 }
+// Você terá que implementar as seguintes conversões:
+
+// • %c Imprime um único caractere.
+
+// • %s Imprime uma string (conforme definido pela convenção comum do C).
+
+// • %p O endereço do ponteiro void * deve ser impresso em formato hexadecimal.
+
+// • %d Imprime um número decimal (base 10).
+
+// • %i Imprime um inteiro em base 10.
+
+// • %u Imprime um número decimal (base 10) sem sinal.
+
+// • %x Imprime um número em formato hexadecimal (base 16) em minúsculas.
+
+// • %X Imprime um número em formato hexadecimal (base 16) em maiúsculas.
+
+// • %% Imprime um sinal de porcentagem.
